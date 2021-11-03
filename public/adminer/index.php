4e69f07776a803605775fe88644f1eddbd194db2
Dekpo <dekpo@me.com>
2021-11-03 14:51:44 +0100

    Added a route in PictureController to display getAll() method using Json

---
+++ public/adminer/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$_GET['sqlite'] = '';
+
+function adminer_object() {
+
+   require "./plugins/fc-sqlite-connection-without-credentials.php";
+   require "./plugins/plugin.php";
+  
+   $plugins = array(new FCSqliteConnectionWithoutCredentials());
+    
+   return new AdminerPlugin($plugins);
+
+}
+
+require "./adminer.php";
--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -2,12 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PictureController extends AbstractController
 {
+    private $pictureRepository;
+    public function __construct(PictureRepository $pictureRepository)
+    {
+       $this->pictureRepository = $pictureRepository;
+    }
+     /**
+     * @Route("/pictures", name="pictures")
+     */
+    public function getAll(): JsonResponse
+    {
+       $pictures = $this->pictureRepository->findAll();
+       $data = [];
+       foreach($pictures as $pic){
+           $data[] = [
+               'id'     => $pic->getId(),
+               'url'    => $pic->getUrl(),
+               'author' => $pic->getAuthor(),
+               'title'  => $pic->getTitle(),
+               'description' => $pic->getDescription()
+           ];
+       }
+       return new JsonResponse($data,Response::HTTP_OK);
+    }
     /**
      * @Route("/picture", name="picture")
      */
